4e10dce8489da75c46c87ad898fc1fea564c653b
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2022-09-10 15:59:23 +0200

    Update Bluehack.py

--- Bluehack.py
+++ Bluehack.py
@@ -9,20 +9,20 @@ def banner():
     print("""|  | |   |    | |   |   |  /--\ |     | \  """)
     print(""" --  |__  ----   -- |   | /    \ ---- |  \ """)
 def opt(): # options code
-   mac = input("remote mac --> ")
-   print("Scan     [ 1 ]")
+   print("Scan        [ 1 ]")
    print("Dos         [ 2 ]")
    print("exit        [ 3 ]")
    opt = input("[ Bluehack ] --> ")
    if opt == 1:
-       listen(mac)
+       listen()
    elif opt == 2:
-       send(mac)
+       send()
    else:
        sys.exit()
-def listen(mac): # listen code
+def listen(): # listen code
     os.system("hcitool scan")
-def send(mac): # Dos code
+def send(): # Dos code
+    mac = input("remote mac -->")
     try:
         os.system("sudo hcitool cc {} ; sudo hcitool auth {}".format(mac, mac))
     except Eception:
